01751405095debd5c079d24eebc06d0d6e30f998
KGCybeX <k4gcybex@gmail.com>
2023-08-24 09:35:17 +0200

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dialpad/flutter_dialpad.dart';
 
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -11,27 +14,27 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
+  void _makeCall(String number) {
+    print('calling $number');
+  }
+
+  void _keyPressed(String number) {
+    print(number);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
         body: SafeArea(
-            child:
-            DialPad(
-                enableDtmf: true,
-                //outputMask: "(000) 000-0000",
-                hideSubtitle: false,
-                backspaceButtonIconColor: Colors.red,
-                buttonTextColor: Colors.white,
-                dialOutputTextColor: Colors.white,
-                keyPressed: (value){
-                  print('$value was pressed');
-                },
-                makeCall: (number){
-                  print(number);
-                }
-            )
+            child: DialPad(
+              makeCall: _makeCall,
+              keyPressed: _keyPressed,
+              dialButtonColor: Colors.blue,
+              buttonColor: Colors.white,
+              buttonTextColor: Colors.black54,
+            ),
         ),
       ),
     );
