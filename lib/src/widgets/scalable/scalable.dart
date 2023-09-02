d49c09601d6cd299a68b72daa413aaf0a2b550c6
KGCybeX <k4gcybex@gmail.com>
2023-09-02 23:19:51 +0200

    Add Scalable Widget

---
+++ lib/src/widgets/scalable/scalable.dart
@@ -0,0 +1,4 @@
+export 'scalable_button.dart';
+export 'button_type.dart';
+export 'scaling_type.dart';
+export 'scaling_size.dart';
---
+++ lib/src/widgets/scalable/scaling_size.dart
@@ -0,0 +1,42 @@
+/// Scaling Factor for the [ScalableButton]
+enum ScalingSize {
+  /// Scalar value: 0.0003
+  smallest,
+
+  /// Scalar value: 0.0006
+  smaller,
+
+  /// Scalar value: 0.001
+  small,
+
+  /// Scalar value: 0.003
+  medium,
+
+  /// Scalar value: 0.006
+  large,
+
+  /// Scalar value: 0.01
+  veryLarge,
+
+  /// Scalar value: 0.05
+  gigantic;
+
+  double get scalar {
+    switch (this) {
+      case ScalingSize.smallest:
+        return 0.0003;
+      case ScalingSize.smaller:
+        return 0.0006;
+      case ScalingSize.small:
+        return 0.001;
+      case ScalingSize.medium:
+        return 0.003;
+      case ScalingSize.large:
+        return 0.006;
+      case ScalingSize.veryLarge:
+        return 0.01;
+      case ScalingSize.gigantic:
+        return 0.05;
+    }
+  }
+}
